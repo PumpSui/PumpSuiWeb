b80c09bf03c89277549fcf1cdcdcbc55a514bbf2
Wulabalabo <cyberchenjw@gmail.com>
2024-07-09 23:33:12 +0800

    [fix] mainpage layout

--- web/app/(main)/page.tsx
+++ web/app/(main)/page.tsx
@@ -18,7 +18,7 @@ const Page: React.FC = () => {
   return (
     <main>
       <div className="py-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 px-4">
           {records.map((project, index) => (
             <ProjectCard key={index} {...project} />
           ))}
