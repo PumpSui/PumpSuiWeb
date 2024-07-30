55a17198057a2df95636fedc4a66a55049b33c1d
Wulabalabo <cyberchenjw@gmail.com>
2024-07-30 11:11:38 +0800

    [fix] remove edit page layout

--- web/app/(main)/project/[projectId]/edit/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { useParams } from "next/navigation";
-
-const Page = () => {
-  const { projectId } = useParams();
-  return <h1>Edit {projectId}</h1>;
-};
-
-export default Page;
