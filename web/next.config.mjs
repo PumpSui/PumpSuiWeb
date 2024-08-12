1d3d9faccb896b9895cdc3fad30aa032e42757cb
Wulabalabo <cyberchenjw@gmail.com>
2024-08-13 01:18:36 +0800

    [feat] remove console when publish

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,20 +1,18 @@
 /** @type {import('next').NextConfig} */
-import TerserPlugin from 'terser-webpack-plugin';
-
 const nextConfig = {
-    images: {
-        remotePatterns: [
+    images:{
+        remotePatterns:[
             {
                 protocol: 'https',
-                hostname: 'svgshare.com'
+                hostname:'svgshare.com'
             },
             {
-                protocol: 'https',
-                hostname: 'images.contentstack.io'
+                protocol:'https',
+                hostname:'images.contentstack.io'
             },
             {
-                protocol: 'https',
-                hostname: 'imgur.com'
+                protocol:'https',
+                hostname:'imgur.com'
             },
             {
                 protocol: 'https',
@@ -26,21 +24,11 @@ const nextConfig = {
             }
         ]
     },
-    webpack: (config, { isServer, dev }) => {
-        if (!dev && !isServer) {
-            config.optimization.minimizer = config.optimization.minimizer || [];
-            config.optimization.minimizer.push(
-                new TerserPlugin({
-                    terserOptions: {
-                        compress: {
-                            drop_console: true,
-                        },
-                    },
-                })
-            );
+    compiler:{
+        removeConsole:{
+            exclude: ['error']
         }
-        return config;
-    },
+    }
 };
 
 export default nextConfig;
